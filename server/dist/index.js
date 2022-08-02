fd968b2f99ddac675ad97e160a05d7039f78cc02
Emil-Ka <emilkaraev55@gmail.com>
2022-08-02 16:21:07 +0400

    main начальная настройка проекта

---
+++ server/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const db_config_1 = require("./config/db.config");
+console.log(db_config_1.a);
+//# sourceMappingURL=index.js.map
